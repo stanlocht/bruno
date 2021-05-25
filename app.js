13104d158fbada590b89ad47d942aba07c52b38f
stanlochten <stanlochtenberg@gmail.com>
2021-05-26 00:25:34 +0200

    haha

---
+++ app.js
@@ -0,0 +1,9 @@
+ 
+let wave = new Wave(); 
+options = {type:"bars"};
+wave.fromElement("speelplein","output", options);
+
+
+
+
+// colors:["rgb(136, 110, 26)", "rgb(187, 161, 77)", "rgb(199, 178, 108)"]
